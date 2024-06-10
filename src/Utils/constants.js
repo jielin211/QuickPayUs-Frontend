8f6febde66f58d21a58b3ffaf42f61d0240e8868
Johaner <providencesatterfield@gmail.com>
2024-06-10 10:06:24 +0900

    set border color according to the document

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,11 +1,11 @@
 export const lightTheme = {
   colorBgListItemActived: "rgba(0, 0, 0, 0.06)",
-  colorBorderPrimary: "#d9d9d9",
+  colorBorderPrimary: "#D3D3D3",
   colorBgLayout: "#f5f5f5",
 };
 
 export const darkTheme = {
   colorBgListItemActived: "rgba(255, 255, 255, 0.12)",
-  colorBorderPrimary: "#424242",
+  colorBorderPrimary: "#2A2A2A",
   colorBgLayout: "#000000",
 };
